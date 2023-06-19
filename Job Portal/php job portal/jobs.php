eddffd85f501dca717025aacb28ea5cabdca3c4e
Anshikasinghbhadauriya <114208295+Anshikasinghbhadauriya@users.noreply.github.com>
2023-06-19 12:01:14 +0530

    Add files via upload

---
+++ Job Portal/php job portal/jobs.php
@@ -0,0 +1,25 @@
+<?php include "header.php" ?>
+<div class="content">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Candidate Name</th>
+            <th scope="col">Position</th>
+            <th scope="col">Resume</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Ram</td>
+            <td>PHP Devloper</td>
+            <td><a href=""></a><i class="fa-fa download" aria- hidden="true"></i></a>></td>
+          </tr>
+         
+    
+        </tbody>
+      </table> 
+
+
+</div>
